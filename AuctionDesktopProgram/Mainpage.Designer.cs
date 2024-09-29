1f3d9833f538762d9f2a72c00e064f80b5db8ab2
Blas Cabrera <blascabrera78@gmail.com>
2024-09-29 11:33:57 -0300

    menu principal

---
+++ AuctionDesktopProgram/Mainpage.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace AuctionDesktopProgram
+{
+    partial class Mainpage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.FromArgb(255, 128, 0);
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(438, 37);
+            label1.Name = "label1";
+            label1.Size = new Size(276, 63);
+            label1.TabIndex = 0;
+            label1.Text = "TUP-SUBASTAS";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 128, 0);
+            button1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(157, 171);
+            button1.Name = "button1";
+            button1.Size = new Size(241, 36);
+            button1.TabIndex = 1;
+            button1.Text = "CREAR SUBASTA";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(255, 128, 0);
+            button2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(157, 283);
+            button2.Name = "button2";
+            button2.Size = new Size(241, 36);
+            button2.TabIndex = 2;
+            button2.Text = "RESUMEN DE PRODUCTOS";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(255, 128, 0);
+            button3.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(740, 171);
+            button3.Name = "button3";
+            button3.Size = new Size(222, 36);
+            button3.TabIndex = 3;
+            button3.Text = "EDITAR SUBASTA";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(255, 128, 0);
+            button4.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.Location = new Point(740, 283);
+            button4.Name = "button4";
+            button4.Size = new Size(222, 36);
+            button4.TabIndex = 4;
+            button4.Text = "INFORMES";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // Mainpage
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(1154, 580);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Mainpage";
+            Text = "Mainpage";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
